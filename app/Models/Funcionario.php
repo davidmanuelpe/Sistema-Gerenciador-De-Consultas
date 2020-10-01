7d655ec7d99456a568a0c3e65cf10eee889fe9a9
davidmanuelpe <davidmanuelpe@gmail.com>
2020-10-01 02:42:22 -0300

    Validaçao da entidade Recepcionista, e atualizaçao da validaçao funcionario

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -12,14 +12,14 @@ class Funcionario extends Model
     protected $fillable = ['carga_horaria', 'tipo', 'funcionarioable_type', 'funcionarioable_id'];
 
     public static $rules = ['carga_horaria' => 'required',
-                            'tipo' => 'required|min:6',
+                            'tipo' => 'required|min:11',
                             'funcionarioable_type' => 'required|min:12', 
                             'funcionarioable_id' => 'required'];
 
     public static $messages = ['carga_horaria.*' => 'O campo de carga horário é obrigatório',
                                'tipo.*' => 'O campo de cargo é obrigatório',
-                               'funcionarioable_type' => 'Tipo de funcionário deve existir', 
-                               'funcionarioable_id' => 'id de funcionário deve existir'];
+                               'funcionarioable_type.*' => 'Tipo de funcionário deve existir', 
+                               'funcionarioable_id.*' => 'id de funcionário deve existir'];
 
     public function pessoa(){
         return $this->morphOne(Pessoa::class, 'pessoaable');
