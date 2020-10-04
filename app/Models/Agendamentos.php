fe9abc263f90144d064158e00f97c9d170802054
Programação web <noronhamatheus07@gmail.com>
2020-10-04 17:56:34 -0300

    validaçao de dados

--- app/Models/Agendamentos.php
+++ app/Models/Agendamentos.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Agendamentos extends Model
 {
     use HasFactory;
+
+    use Notifiable;
+
+    protected $fillable = ['agenda_id', 'paciente_id', 'data'];
+
+    public static $rules = [
+        'agenda_id' => 'required',
+        'paciente_id' => 'required',
+        'data' => 'required|date',
+    ];
+    
+    public static $messages = [
+        'agenda_id.*' => 'O deve referenciar uma agenda de um medico.',
+        'paciente_id.*' => 'O deve referenciar um paciente.',
+
+    ];
 
     public function agenda(){
         return $this->belongsTo('App\Models\Agenda');
