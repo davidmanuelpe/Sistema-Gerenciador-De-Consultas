9f5e72d41d3ed772dda5630825d6c72e1c77c1fd
davidmanuelpe <davidmanuelpe@gmail.com>
2020-10-14 03:16:09 -0300

    editar agenda/horario feito

--- app/Http/Controllers/RecepcionistaAgendasController.php
+++ app/Http/Controllers/RecepcionistaAgendasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Medico;
+use App\Models\Horario;
 use App\Models\Recepcionista;
 use Illuminate\Http\Request;
 
@@ -22,5 +23,39 @@ class RecepcionistaAgendasController extends Controller
     public function show(Request $request){
         $medico = Medico::find($request['medico_id']);
         return view('recepcionista/mostraragenda', ['medico' => $medico]);
+    }
+
+    public function edit($id){
+        $horario = Horario::find($id);
+        $hora_inicio = $horario->horario_inicio[0] . $horario->horario_inicio[1];
+        $minuto_inicio = $horario->horario_inicio[3] . $horario->horario_inicio[4];
+        $hora_final = $horario->horario_fim[0] . $horario->horario_fim[1];
+        $minuto_final = $horario->horario_fim[3] . $horario->horario_fim[4];
+        $dias = collect(['Segunda-feira', 'Terça-feira', 'Quarta-feira',
+                         'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']);
+        return view('recepcionista/editagenda', ['horario' => $horario, 'dias' => $dias, 'hora_inicio' => $hora_inicio,
+                                                 'minuto_inicio' => $minuto_inicio, 'hora_final' => $hora_final, 'minuto_final' => $minuto_final]);
+
+    }
+
+    protected function update(Request $request){
+        $inicio = $request['hora_inicio'] . ':' . $request['minuto_inicio'] . ':' . '00';
+        $final = $request['hora_final'] . ':' . $request['minuto_final'] . ':' . '00';
+
+        $horario = Horario::find($request['id']);
+        $agenda_id = $horario->agenda->id;
+        
+        $horario->update([
+            'horario_inicio' => $inicio,
+            'horario_fim' => $final,
+            'dia_semana' => $request['dia_semana'],
+            'agenda_id' => $agenda_id
+        ]);
+        return redirect('recepcionista/mostraragenda');
+    }
+
+    protected function destroy($id){
+        Horario::destroy($id);
+        return redirect('/');
     }
 }
---
+++ resources/views/recepcionista/editagenda.blade.php
@@ -0,0 +1,178 @@
+@extends('layouts.app')
+
+<html>
+    <head>
+        <script>
+
+        </script>
+    </head>
+</html>    
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Editar Agenda') }}</div>
+
+                <div class="card-body">
+                    <form method="post" action="{{ url('editaragenda') }}">
+                        @csrf
+
+                        <div class="form-group row" id="Dias" style="display: none">
+                            <label for="dia_semana" class="col-md-4 col-form-label text-md-right">{{ __('Id_Horario') }}</label>
+                            <div class="col-md-6">
+                        <select class="custom-select{{ $errors->has('dia_semana') ? ' is-invalid' : '' }}" id="id" name='id' required autocomplete="id">
+                            <option value="{{$horario->id }}">{{$horario->id }}</option>
+                        </select>
+                        </div>
+                        </div>
+                    
+                        <div class="form-group row" id="Dias">
+                            <label for="dia_semana" class="col-md-4 col-form-label text-md-right">{{ __('Dia da semana') }}</label>
+                            <div class="col-md-6">
+                        <select class="custom-select{{ $errors->has('dia_semana') ? ' is-invalid' : '' }}" id="dia_semana" name='dia_semana' required autocomplete="dia_semana">
+                            <option value="{{$horario->dia_semana }}">{{$horario->dia_semana }}</option>
+                        @foreach ($dias as $item)
+                            <option value="{{$item}}">{{$item}}</option>
+                        @endforeach
+                        </select>
+                        @if($errors->has('dia_semana'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('dia_semana')}}
+                            </div>
+                        @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row" id="inicio">
+                            <label for="inicio" class="col-md-7 col-form-label text-right">{{ __('Horário de inicio do turno') }}</label>
+                        </div>
+
+                        <div class="form-group row" id="horas_inicio">
+                            <label for="hora_inicio" class="col-md-2 col-form-label text-md-right">{{ __('Hora') }}</label>
+                            <div class="col-md-3">
+                        <select class="custom-select{{ $errors->has('hora_inicio') ? ' is-invalid' : '' }}" id="hora_inicio" name='hora_inicio'>
+                            @if((Int)($hora_inicio) < 2) 
+                                <option value="{{$hora_inicio}}">{{$hora_inicio}} Hora</option>
+                            @else
+                                <option value="{{$hora_inicio}}">{{$hora_inicio}} Horas</option>
+                            @endif
+                            <option value="{{0 . 0}}">00 Hora</option>
+                            <option value="{{0 . 1}}">01 Hora</option>
+                        @foreach (range(2, 23) as $item)
+                            @if((Int)($item) < 10) 
+                            <option value="{{0 . $item}}">{{'0' . $item}} Horas</option>
+                            @else
+                            <option value="{{$item}}">{{$item}} Horas</option>
+                            @endif
+                        @endforeach
+                        </select>
+                        @if($errors->has('hora_inicio'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('hora_inicio')}}
+                            </div>
+                        @endif
+                            </div>
+
+                            <label for="minuto_inicio" class="col-right-2 col-form-label text-md-right">{{ __('Minuto') }}</label>
+                            <div class="col-md-3">
+                        <select class="custom-select{{ $errors->has('minuto_inicio') ? ' is-invalid' : '' }}" id="minuto_inicio" name='minuto_inicio'>
+                            @if((Int)($minuto_inicio) < 2) 
+                                <option value="{{$minuto_inicio}}">{{$minuto_inicio}} Minuto</option>
+                            @else
+                                <option value="{{$minuto_inicio}}">{{$minuto_inicio}} Minutos</option>
+                            @endif
+                        <option value="{{0 . 0}}">00 Minuto</option>
+                        <option value="{{0 . 1}}">01 Minuto</option>
+                        @foreach (range(2, 59) as $item)
+                            @if((Int)($item) < 10) 
+                            <option value="{{0 . $item}}">{{'0' . $item}} Minutos</option>
+                            @else
+                            <option value="{{$item}}">{{$item}} Minutos</option>
+                            @endif
+                        @endforeach
+                        </select>
+                        @if($errors->has('minuto_inicio'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('minuto_inicio')}}
+                            </div>
+                        @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row" id="final">
+                            <label for="final" class="col-md-7 col-form-label text-right">{{ __('Horário de final do turno') }}</label>
+                        </div>
+
+                        <div class="form-group row" id="hora_final">
+                            <label for="hora_final" class="col-md-2 col-form-label text-md-right">{{ __('Hora') }}</label>
+                            <div class="col-md-3">
+                        <select class="custom-select{{ $errors->has('hora_final') ? ' is-invalid' : '' }}" id="hora_final" name='hora_final'>
+                            @if((Int)($hora_final) < 2) 
+                                <option value="{{$hora_final}}">{{$hora_final}} Hora</option>
+                            @else
+                                <option value="{{$hora_final}}">{{$hora_final}} Horas</option>
+                            @endif
+                            <option value="{{0 . 0}}">00 Hora</option>
+                            <option value="{{0 . 1}}">01 Hora</option>
+                        @foreach (range(2, 23) as $item)
+                            @if((Int)($item) < 10) 
+                            <option value="{{0 . $item}}">{{'0' . $item}} Horas</option>
+                            @else
+                            <option value="{{$item}}">{{$item}} Horas</option>
+                            @endif
+                        @endforeach
+                        </select>
+                        @if($errors->has('hora_final'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('hora_final')}}
+                            </div>
+                        @endif
+                            </div>
+
+                            <label for="minuto_final" class="col-right-2 col-form-label text-md-right">{{ __('Minuto') }}</label>
+                            <div class="col-md-3">
+                        <select class="custom-select{{ $errors->has('minuto_final') ? ' is-invalid' : '' }}" id="minuto_final" name='minuto_final'>
+                            @if((Int)($minuto_final) < 2) 
+                                <option value="{{$minuto_final}}">{{$minuto_final}} Minuto</option>
+                            @else
+                                <option value="{{$minuto_final}}">{{$minuto_final}} Minutos</option>
+                            @endif
+                            <option value="{{0 . 0}}">00 Minuto</option>
+                            <option value="{{0 . 1}}">01 Minuto</option>
+                        @foreach (range(2, 59) as $item)
+                            @if((Int)($item) < 10) 
+                            <option value="{{0 . $item}}">{{'0' . $item}} Minutos</option>
+                            @else
+                            <option value="{{$item}}">{{$item}} Minutos</option>
+                            @endif
+                        @endforeach
+                        </select>
+                        @if($errors->has('minuto_final'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('minuto_final')}}
+                            </div>
+                        @endif
+                            </div>
+                        </div>
+
+                        
+                        <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Editar Horário') }}
+                            </button>
+                        </div>
+                    </div>        
+                    </div>
+
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>    
+
+@endsection
